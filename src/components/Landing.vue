52a1bebcb37c417cee769f61ccf870a2ab00819c
Fathursyah Awandanu <fathur.syah1@gmail.com>
2024-12-23 19:39:26 +0700

    first commit

---
+++ src/components/Landing.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <div
+  class="hero min-h-screen bg-[url('/landing_bg.jpg')]">
+  <div class="hero-overlay bg-opacity-80"></div>
+  <aos-vue animation="fade" :delay="500" :duration="600" :once="true">
+    <div class="hero-content text-white text-center">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold font-IBM">Cafe Finder</h1>
+        <p class="mb-5">
+          Tempat terpercaya untuk mendapatkan rekomendasi kafe di sekitar Universitas Pakuan guna kebutuhan hangout.
+        </p>
+        <router-link to="/home">
+          <button class="btn btn-primary text-white px-12">Get Started</button>
+        </router-link>
+      </div>
+    </div>
+  </aos-vue>
+</div>
+  </main>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <h1 class="text-pink-600">hello kawan</h1>
+    <router-link to="/">
+      <button class="btn btn-warning">Accent</button>
+    </router-link>
+
+  </main>
+</template>
